71e81430a4734162b7e2520bc5bf4d5e313ce721
pepema <petter.emanuelsson@gmail.com>
2020-10-19 18:03:34 +0200

    Fixed bonus assignment and added assignment descriptions into a markdown

---
+++ Lecture1/assignments.md
@@ -0,0 +1,6 @@
+1. Write a program which takes a positive number from user, and then fill in a 3-dimensional array of size [8,9,11]
+with random positive numbers smaller than the one user has provided  
+Pretty print the array.  
+2. Implement the decryption function of Caeser Cipher with custom length key and decrypt the following
+the application should ask for a key between (0-26), and then the cipher text, and shall print the decryption.  
+3. Bonus - Write a program which can guess the number that user is thinking of – between 1-128 – you can ask anything you want up to 7 times.
--- Lecture1/bonus.cpp
+++ Lecture1/bonus.cpp
@@ -3,37 +3,37 @@ using namespace std;
 
 int askQuestion(int index){
   int bit_value;
-  cout << "Is the number " << index << " bit of your number 1? (y/n)";
+  cout << "From right, is the number " << index << " bit of your number 1? (y/n)" << endl;
   string ans;
   cin >> ans;
   if(ans == "y"){
-    bit_value << index;
+    bit_value = 1;
   }
   else{
-    bit_value << index;
+    bit_value = 0;
   }
-  cout << bit_value << endl;
-  return bit_value;
+  return bit_value << index;
 }
 
 int main(){
 
   cout << "Think of a number between 1 and 128 and i will guess it" << endl;
  
-  int ans;
-  int current_number;
-  string correct_number;
+  int ans = 0;
+  int correct_number = 0;
   for(int i = 0; i<7; i++){
     ans = askQuestion(i);
+    correct_number += ans;
   }
-  cout << correct_number << endl;
-  cout << "Was this your number? " << correct_number << " (y/n)" << endl;
+  if(correct_number == 0){
+    correct_number = 128;
+  }
 
+  cout << "Was this your number? " << correct_number << " (y/n)" << endl;
   string answer;
   cin >> answer;
-
   if(answer=="y"){
-    cout << "Great succes" << endl;
+    cout << "Great success" << endl;
   }
   else{
     cout << "You're lying..." << endl;
