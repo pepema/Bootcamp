40b46fdc279eead58b7ad4d1b5091bb5da0f0893
pepema <petter.emanuelsson@gmail.com>
2020-10-22 19:42:58 +0200

    beautification

--- Lecture4/Bonus/Yesterdays/storage_car_shop.cpp
+++ Lecture4/Bonus/Yesterdays/storage_car_shop.cpp
@@ -5,36 +5,36 @@ int main(){
   Date date_;
   Payment payment_;
   Service service_;
-  Customer customer_;
+  Customer record_;
   RecordList list_;
 
   part_ = Part(Parts::Trunk);
   date_ = Date(94,11,14);
   payment_ = Payment(15000,PaymentMethods::Phone);
-  customer_ = Customer("frank");
-  Record one = Record(date_,payment_,part_,customer_);
+  record_ = Customer("frank");
+  Record one = Record(date_,payment_,part_,record_);
 
   part_ = Part(Parts::Engine);
   date_ = Date(93,05,02);
   payment_ = Payment(1410065407,PaymentMethods::Cash);
-  customer_ = Customer("ludi");
-  Record two = Record(date_,payment_,part_,customer_);
+  record_ = Customer("ludi");
+  Record two = Record(date_,payment_,part_,record_);
 
   service_ = Service(Services::OilChange);
   date_ = Date(20,10,21);
   payment_ = Payment(2,PaymentMethods::Card);
-  customer_ = Customer("boss");
-  Record three = Record(date_,payment_,service_,customer_);
+  record_ = Customer("boss");
+  Record three = Record(date_,payment_,service_,record_);
 
   service_ = Service(Services::FilterChange);
   part_ = Part(Parts::Hood);
   date_ = Date(87,04,13);
   payment_ = Payment(666,PaymentMethods::Card);
-  customer_ = Customer("nisse");
-  Record four = Record(date_,payment_,service_,part_,customer_);
+  record_ = Customer("nisse");
+  Record four = Record(date_,payment_,service_,part_,record_);
 
-  customer_ = Customer("nisse");
-  Record five = Record(date_,payment_,service_,part_,customer_);
+  record_ = Customer("nisse");
+  Record five = Record(date_,payment_,service_,part_,record_);
   list_.addItem(one);
   list_.addItem(two);
   list_.addItem(three);
